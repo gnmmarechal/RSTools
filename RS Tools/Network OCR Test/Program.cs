9f06fda6f96ffeeee23c2ce48596c6268d55bd28
Mario Alexandre Lopes Liberato <mliberato@gs2012.xyz>
2019-04-23 15:30:36 +0100

    Network OCR now works (support yet to be added to RS Tools)

---
+++ RS Tools/Network OCR Test/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network_OCR_Test
+{
+    class Program
+    {
+        static readonly string ip = "127.0.0.1";
+        static readonly int port = 49153;
+        static void Main(string[] args)
+        {
+
+            //---create a TCPClient object at the IP and port no.---
+            TcpClient client = new TcpClient(ip, port);
+            NetworkStream nwStream = client.GetStream();
+            Bitmap testBmp = new Bitmap("playNow.bmp");
+            byte[] imageBytes = ImageToByte(testBmp);
+            byte[] imageSize = BitConverter.GetBytes(imageBytes.Length);
+            byte[] bytesToSend = new byte[imageBytes.Length + imageSize.Length];
+
+            imageSize.CopyTo(bytesToSend, 0);
+            imageBytes.CopyTo(bytesToSend, imageSize.Length);
+            Console.WriteLine("MESSAGE SIZE: " + (bytesToSend.Length - imageBytes.Length));
+            Console.WriteLine("IMAGE SIZE:" + imageBytes.Length);
+            //Console.WriteLine("[{0}]", string.Join(", ", bytesToSend));
+
+            //---send the text---
+            Console.WriteLine("Sending bmp...");
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+            //---read back the text---
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+            Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+            Console.ReadLine();
+            client.Close();
+        }
+
+        public static byte[] ImageToByte(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Bmp);
+                return stream.ToArray();
+            }
+        }
+    }
+}
--- RS Tools/RSTExternalOCR/Program.cs
+++ RS Tools/RSTExternalOCR/Program.cs
@@ -1,29 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using RS_Tools;
+using Tesseract;
 
 namespace RSTExternalOCR
 {
     class Program
     {
-        static string ip = "127.0.0.1";
-        static int port = 49153;
+        static readonly string ip = "127.0.0.1";
+        static readonly int port = 49153;
 
         static void Main(string[] args)
         {
+            Display.eng = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default);
+
             bool running = true;
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(ip);
+            TcpListener listener = new TcpListener(localAdd, port);
+            Console.WriteLine("Listening...");
+            listener.Start();
             while (running)
             {
-                Console.WriteLine("Waiting for image...");
-                Image bmp = ReceiveBitmap(ip, port);
-                Console.WriteLine("Received image!");
+                //---incoming client connected---
+                TcpClient client = listener.AcceptTcpClient();
+
+                //---get the incoming data through a network stream---
+                NetworkStream nwStream = client.GetStream();
+                byte[] buffer = new byte[sizeof(Int32)];
+                int bytesRead = nwStream.Read(buffer, 0, sizeof(Int32));
+                Console.WriteLine("[{0}]", string.Join(", ", buffer));
+                // Receive data size
+                int imageSize = BitConverter.ToInt32(buffer, 0);
+                Console.WriteLine("IMAGE SIZE: " + imageSize);
+
+                buffer = new byte[imageSize];
+                bytesRead = nwStream.Read(buffer, 0, imageSize);
+                
+
+                //---convert the data received into a string---
+                Bitmap original = null;
+                //Console.WriteLine("[{0}]", string.Join(", ", buffer));
+                var ms = new MemoryStream(buffer);
+                ms.Seek(0, SeekOrigin.Begin);
+                original = new Bitmap(ms);
+                //original.Save("tets.bmp");
+                //original = (Bitmap)Image.FromStream(ms);
+
+                Bitmap copy = new Bitmap(original.Width, original.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                using (Graphics graphics = Graphics.FromImage(copy))
+                {
+                    graphics.DrawImage(original, new Point(0, 0));
+                }
+                ms.Close();
+                //copy.Save("ye.bmp");
+                Bitmap bigSc = Display.ResizeImage(copy, copy.Width * 5, copy.Height * 5);
+                Bitmap bigSc2 = Display.AdjustContrast(bigSc, 40);
+
+
+                //string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                //Console.WriteLine("Received : " + dataReceived);
+
+                //---write back the text to the client---
+                //Console.WriteLine("Sending back : " + dataReceived);
+                string reply = Display.GetText(bigSc2);
+                Console.WriteLine(reply);
+                buffer = Encoding.ASCII.GetBytes(reply);
+                nwStream.Write(buffer, 0, buffer.Length);
+                client.Close();
+                copy.Dispose();
+                original.Dispose();
+                bigSc.Dispose();
+                bigSc2.Dispose();
             }
-            
+            listener.Stop();
+            Console.ReadLine();
+
 
         }
 
