0e73360578b5ce448206d110917732311ac303ca
Mario Alexandre Lopes Liberato <mliberato@gs2012.xyz>
2019-04-23 13:30:01 +0100

    Starting work on the network OCR support

---
+++ RS Tools/RS Tools/Networking.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RS_Tools
+{
+    public class Networking
+    {
+        public static void SendImage(Image obj)
+        {
+            TcpListener listener = new TcpListener(49153);
+            listener.Start();
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+
+            Image image = obj;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, ImageFormat.Bmp);
+                byte[] imageBuffer = ms.GetBuffer();
+                stream.Write(imageBuffer, 0, (int)ms.Length);
+            }
+
+            stream.Close(500);
+            client.Close();
+            listener.Stop();
+        }
+    }
+}
---
+++ RS Tools/RSTExternalOCR/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RSTExternalOCR
+{
+    class Program
+    {
+        static string ip = "127.0.0.1";
+        static int port = 49153;
+
+        static void Main(string[] args)
+        {
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("Waiting for image...");
+                Image bmp = ReceiveBitmap(ip, port);
+                Console.WriteLine("Received image!");
+            }
+            
+
+        }
+
+        public static Image ReceiveBitmap(string ip, int port)
+        {
+            TcpClient client = new TcpClient();
+            bool connected = false;
+
+            while (!connected)
+            {
+                try
+                {
+                    client.Connect(ip, port);
+                    connected = true;
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Could not connect. Retrying in 5 seconds...");
+                    Thread.Sleep(5000);
+                }
+            }
+
+            Console.WriteLine("Connected!");
+            NetworkStream stream = client.GetStream();
+
+            Image image = Image.FromStream(stream);
+
+            stream.Close();
+            client.Close();
+
+            return image;
+        }
+    }
+
+
+}
